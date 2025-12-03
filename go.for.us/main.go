c95727aedb899d3eb6e67b5c177576e65f5eba12
ananastya130 <hipeall60@gmail.com>
2025-12-03 20:16:28 +0700

    Add files via upload

---
+++ go.for.us/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Читаем настройки из env или берём дефолты
+	pgHost := getenv("PGHOST", "localhost")
+	pgPort := getenv("PGPORT", "5433")
+	pgDB := getenv("PGDB", "musicdb")
+	pgUser := getenv("PGUSER", "musicuser")
+	pgPass := getenv("PGPASS", "m0")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		pgHost, pgPort, pgUser, pgPass, pgDB)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("db open: %v", err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatalf("db ping: %v", err)
+	}
+
+	app := &App{DB: db}
+
+	r := gin.Default()
+	// шаблоны
+	r.SetFuncMap(template.FuncMap{})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/static", "./static")
+
+	r.GET("/", app.indexHandler)
+	r.GET("/api/search", app.searchHandler)
+
+	port := getenv("PORT", "8080")
+	log.Printf("Server started at http://localhost:%s", port)
+	r.Run(":" + port)
+}
+
+func getenv(k, def string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		return def
+	}
+	return v
+}
